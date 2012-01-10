1a70a2cf3cf8201c39d20300cc2222af45b251fd
delta.danger@gmail.com <delta.danger@gmail.com@5e4e380e-65ee-864f-d62a-fed095f5eb9d>
2012-01-10 01:11:14 +0000

    Project creation

---
+++ trunk/src/com/piratebox/P1R4T3B0XActivity.java
@@ -0,0 +1,13 @@
+package com.piratebox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class P1R4T3B0XActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
