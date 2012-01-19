78754ccb2311b572e987e5b8dd3544e7c40b651a
delta.danger <delta.danger@gmail.com>
2012-01-19 06:22:04 +0000

    Widget updated correctly Widget click switches server state Server can't stop properly

--- src/com/piratebox/server/Server.java
+++ src/com/piratebox/server/Server.java
@@ -1,6 +1,10 @@
 package com.piratebox.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -8,13 +12,19 @@ import java.util.ArrayList;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.piratebox.R;
 import com.piratebox.widget.P1R4T3B0XWidget;
+import com.piratebox.wifiap.WifiApManager;
 
 public class Server extends Thread {
+
+	public static final String TEMP_SCRIPT = "tmp_script";
+	public static final String IPTABLES = "iptables";
 	
 	public static enum ServerState {
 		STATE_OFF(R.string.widget_system_off),
@@ -35,6 +45,8 @@ public class Server extends Thread {
 	private ServerState state;
 	private int connectedUsers = 0;
 	private Context ctx;
+	private WifiConfiguration config;
+	private WifiConfiguration savedConfig;
 	
 	public Server(Context ctx) {
 		this.ctx = ctx;
@@ -44,21 +56,37 @@ public class Server extends Thread {
 		} catch (IOException e) {
 			Log.e(this.getClass().getName(), e.toString());
 		}
+
+		config = new WifiConfiguration();
+		config.SSID = ServerConfiguration.WIFI_AP_NAME;
 		
 		state = ServerState.STATE_OFF;
 		updateWidgets();
+		
+		start();
 	}
 	
 	public void run() {
 		try{
-			removeConnectedUser();
-			while(true)
+			while(listenSocket != null)
 			{
 				Socket clientSocket = listenSocket.accept();
                 connections.add(new Connection (clientSocket, this));
 			}
+		} catch(IOException e) {
+			Log.e(this.getClass().getName(), e.toString());
 		}
-		catch(IOException e) {
+	}
+	
+	@Override
+	public synchronized void start() {
+		try {
+			super.start();
+			startRedirection();
+			startHotspot();
+			state = ServerState.STATE_WAITING;
+			updateWidgets();
+		} catch (IllegalThreadStateException e) {
 			Log.e(this.getClass().getName(), e.toString());
 		}
 	}
@@ -72,11 +100,11 @@ public class Server extends Thread {
 		} catch (IOException e) {
 			Log.e(this.getClass().getName(), e.toString());
 		}
-		
+
+		stopHotspot();
+		stopRedirection();
 		state = ServerState.STATE_OFF;
 		updateWidgets();
-		
-		stop();
 	}
 	
 	public ServerState getServerState() {
@@ -104,7 +132,7 @@ public class Server extends Thread {
 		updateWidgets();
 	}
 	
-	private void updateWidgets() {
+	public void updateWidgets() {
 		AppWidgetManager mgr = AppWidgetManager.getInstance(ctx);
 		int[] ids = mgr.getAppWidgetIds(new ComponentName(ctx, P1R4T3B0XWidget.class));
 		
@@ -114,5 +142,94 @@ public class Server extends Thread {
 		views.setTextViewText(R.id.widgetlabel, ctx.getString(state.val()));
 		
 		mgr.updateAppWidget(ids, views);
+	}
+
+	private void startRedirection() {
+		try {
+			String iptables = loadIptables();
+			StringBuilder script = new StringBuilder();
+			script.append(iptables).append(" --version\n");
+			// TODO Add lines !!!!
+
+			int res = runScript(script.toString());
+			if (res != 1) {
+				Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			Log.e(this.getClass().getName(), e.toString());
+		}
+	}
+
+	private void stopRedirection() {
+		try {
+			String iptables = loadIptables();
+			StringBuilder script = new StringBuilder();
+			script.append(iptables).append(" --version\n");
+
+			// TODO Add lines !!!!
+
+			int res = runScript(script.toString());
+			if (res != 1) {
+				Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			Log.e(this.getClass().getName(), e.toString());
+		}
+	}
+
+	private String loadIptables() throws IOException {
+		File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+
+		File f = new File(tmpFolder, IPTABLES);
+		f.setExecutable(true);
+		f.deleteOnExit();
+
+		InputStream is = ctx.getResources().openRawResource(R.raw.iptables);
+		byte[] buff = new byte[is.available()];
+		is.read(buff);
+		is.close();
+
+		FileOutputStream out = new FileOutputStream(f);
+		out.write(buff);
+		out.close();
+		return f.getAbsolutePath();
+	}
+
+	private int runScript(String script) throws IOException,
+			InterruptedException {
+
+		File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+
+		File f = new File(tmpFolder, TEMP_SCRIPT);
+		f.setExecutable(true);
+		f.deleteOnExit();
+
+		// Write the script to be executed
+		PrintWriter out = new PrintWriter(new FileOutputStream(f));
+		if (new File("/system/bin/sh").exists()) {
+			out.write("#!/system/bin/sh\n");
+		}
+		out.write(script);
+		if (!script.endsWith("\n")) {
+			out.write("\n");
+		}
+		out.write("exit\n");
+		out.flush();
+		out.close();
+		Process exec = Runtime.getRuntime()
+				.exec("su -c " + f.getAbsolutePath());
+		return exec.waitFor();
+	}
+
+	private void startHotspot() {
+		WifiApManager mgr = new WifiApManager(ctx);
+		savedConfig = mgr.getWifiApConfiguration();
+		mgr.setWifiApEnabled(config, true);
+	}
+
+	private void stopHotspot() {
+		WifiApManager mgr = new WifiApManager(ctx);
+		mgr.setWifiApEnabled(config, false);
+		mgr.setWifiApConfiguration(savedConfig);
 	}
 }
--- src/com/piratebox/widget/P1R4T3B0XWidget.java
+++ src/com/piratebox/widget/P1R4T3B0XWidget.java
@@ -1,36 +1,64 @@
 package com.piratebox.widget;
 
+import com.piratebox.P1R4T3B0XActivity;
+import com.piratebox.R;
+import com.piratebox.server.Server;
+import com.piratebox.server.Server.ServerState;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
-import com.piratebox.R;
-
 public class P1R4T3B0XWidget extends AppWidgetProvider {
+
+	private static final String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		
+		Server server = P1R4T3B0XActivity.getServer();
 		final int N = appWidgetIds.length;
-		// Perform this loop procedure for each App Widget that belongs to this
-		// provider
-		for (int i = 0; i < N; i++) {
-			int appWidgetId = appWidgetIds[i];
-			
-			RemoteViews views = new RemoteViews(context.getPackageName(),
-					R.layout.widget);
-			
+		RemoteViews views = new RemoteViews(context.getPackageName(),
+				R.layout.widget);
+		
+		if (server != null) {
+			server.updateWidgets();
+		} else {
 			String display = context.getString(R.string.widget_system_off);
-			
 			views.setTextViewText(R.id.widgetlabel, display);
-			
+		}
+		
+		for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+            Intent intent = new Intent(context, P1R4T3B0XWidget.class);
+            intent.setAction(ACTION_WIDGET_RECEIVER);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+            views.setOnClickPendingIntent(R.id.widgetlabel, pendingIntent);
+
 			appWidgetManager.updateAppWidget(appWidgetId, views);
 		}
 	}
-	
-//	public void setButtonState(Boolean state) {
-//		
-//	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		if (ACTION_WIDGET_RECEIVER.equals(intent.getAction())) {
+			Server server = P1R4T3B0XActivity.getServer();
+			if (server == null || ServerState.STATE_OFF.equals(server.getServerState())) {
+				server = new Server(context);
+			} else {
+				server.shutdown();
+				server = null;
+			}
+		}
+	}
+
+	// public void setButtonState(Boolean state) {
+	//
+	// }
 }
