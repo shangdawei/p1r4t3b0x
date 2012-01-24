2035b0dd5e8acdaeb4bbc3b78477597d851fce24
delta.danger@gmail.com <delta.danger@gmail.com@5e4e380e-65ee-864f-d62a-fed095f5eb9d>
2012-01-24 00:01:29 +0000

    Added listener for system state (to be tested) Added fields for stats in home page

--- trunk/src/com/piratebox/System.java
+++ trunk/src/com/piratebox/System.java
@@ -5,203 +5,206 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.util.Log;
-import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.piratebox.server.Server;
 import com.piratebox.server.ServerConfiguration;
 import com.piratebox.utils.Callback;
-import com.piratebox.widget.P1R4T3B0XWidget;
 import com.piratebox.wifiap.WifiApManager;
 
 public class System {
-	public static final String TEMP_SCRIPT = "tmp_script";
-	public static final String IPTABLES = "iptables";
-	public static final String PREF_SETTINGS = "settings";
+    public static final String TEMP_SCRIPT = "tmp_script";
+    public static final String IPTABLES = "iptables";
+    public static final String PREF_SETTINGS = "settings";
 
-	private WifiConfiguration config;
-	private WifiConfiguration savedConfig;
-	
-	public static enum ServerState {
-		STATE_OFF(R.string.widget_system_off),
-		STATE_WAITING(R.string.widget_system_waiting),
-		STATE_SENDING(R.string.widget_system_sending);
-		
-		private final int value;
-		private ServerState(int value) {
-			this.value = value;
-		}
-		public int val() {
-			return value;
-		}
-	} 
-	private ServerState state;
-	
-	private Server server;
-	private Context ctx;
-	private Callback callback;
-	
-	private static System instance = null;
-	public static System getInstance(Context ctx) {
-		if (instance == null) {
-			instance = new System(ctx);
-		}
-		return instance;
-	}
-	
-	private System(Context ctx) {
-		this.ctx = ctx;
-		
-		config = new WifiConfiguration();
-		config.SSID = ServerConfiguration.WIFI_AP_NAME;
-		
-		state = ServerState.STATE_OFF;
-		
-		callback = new Callback() {
-			@Override
-			public void call(Object arg) {
-				if ((Integer) arg <= 0) {
-					state = ServerState.STATE_WAITING;
-				} else {
-					state = ServerState.STATE_SENDING;
-				}
-				
-				updateWidgets();
-			}
-		};
-		
-		//TODO create folder if not exists
-	}
-	
-	public void start() {
-		startRedirection();
-		startHotspot();
-		state = ServerState.STATE_WAITING;
-		updateWidgets();
-		
-		server = new Server();
-		server.addConnectedUsersListener(callback);
-		server.start();
-	}
-	
-	public void stop() {
+    private WifiConfiguration config;
+    private WifiConfiguration savedConfig;
 
-		stopHotspot();
-		stopRedirection();
-		state = ServerState.STATE_OFF;
-		updateWidgets();
-		server.stopRun();
-		server = null;
-	}
-	
-	public void updateWidgets() {
-		AppWidgetManager mgr = AppWidgetManager.getInstance(ctx);
-		int[] ids = mgr.getAppWidgetIds(new ComponentName(ctx, P1R4T3B0XWidget.class));
-		
-		RemoteViews views = new RemoteViews(ctx.getPackageName(),
-				R.layout.widget);
+    private ArrayList<Callback> statusChangeListeners = new ArrayList<Callback>();
 
-		views.setTextViewText(R.id.widgetlabel, ctx.getString(state.val()));
-		
-		mgr.updateAppWidget(ids, views);
-	}
+    public static enum ServerState {
+        STATE_OFF(R.string.widget_system_off), STATE_WAITING(R.string.widget_system_waiting), STATE_SENDING(
+                R.string.widget_system_sending);
 
-	private void startRedirection() {
-		try {
-			String iptables = loadIptables();
-			StringBuilder script = new StringBuilder();
-			script.append(iptables).append(" --version\n");
-			// TODO Add lines !!!!
+        private final int value;
 
-			int res = runScript(script.toString());
-			if (res != 1) {
-				Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e) {
-			Log.e(this.getClass().getName(), e.toString());
-		}
-	}
+        private ServerState(int value) {
+            this.value = value;
+        }
 
-	private void stopRedirection() {
-		try {
-			String iptables = loadIptables();
-			StringBuilder script = new StringBuilder();
-			script.append(iptables).append(" --version\n");
+        public int val() {
+            return value;
+        }
+    }
 
-			// TODO Add lines !!!!
+    private ServerState state;
 
-			int res = runScript(script.toString());
-			if (res != 1) {
-				Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
-			}
-		} catch (Exception e) {
-			Log.e(this.getClass().getName(), e.toString());
-		}
-	}
+    private Server server;
+    private Context ctx;
+    private Callback callback;
 
-	private String loadIptables() throws IOException {
-		File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+    private static System instance = null;
 
-		File f = new File(tmpFolder, IPTABLES);
-		f.setExecutable(true);
-		f.deleteOnExit();
+    public static System getInstance(Context ctx) {
+        if (instance == null) {
+            instance = new System(ctx);
+        }
+        return instance;
+    }
 
-		InputStream is = ctx.getResources().openRawResource(R.raw.iptables);
-		byte[] buff = new byte[is.available()];
-		is.read(buff);
-		is.close();
+    private System(Context ctx) {
+        this.ctx = ctx;
 
-		FileOutputStream out = new FileOutputStream(f);
-		out.write(buff);
-		out.close();
-		return f.getAbsolutePath();
-	}
+        config = new WifiConfiguration();
+        config.SSID = ServerConfiguration.WIFI_AP_NAME;
 
-	private int runScript(String script) throws IOException,
-			InterruptedException {
+        setServerState(ServerState.STATE_OFF);
 
-		File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+        callback = new Callback() {
+            @Override
+            public void call(Object arg) {
+                if ((Integer) arg <= 0) {
+                    setServerState(ServerState.STATE_WAITING);
+                } else {
+                    setServerState(ServerState.STATE_SENDING);
+                }
+            }
+        };
 
-		File f = new File(tmpFolder, TEMP_SCRIPT);
-		f.setExecutable(true);
-		f.deleteOnExit();
+        // TODO create folder if not exists
+    }
 
-		// Write the script to be executed
-		PrintWriter out = new PrintWriter(new FileOutputStream(f));
-		if (new File("/system/bin/sh").exists()) {
-			out.write("#!/system/bin/sh\n");
-		}
-		out.write(script);
-		if (!script.endsWith("\n")) {
-			out.write("\n");
-		}
-		out.write("exit\n");
-		out.flush();
-		out.close();
-		Process exec = Runtime.getRuntime()
-				.exec("su -c " + f.getAbsolutePath());
-		return exec.waitFor();
-	}
+    public void start() {
+        startRedirection();
+        startHotspot();
+        setServerState(ServerState.STATE_WAITING);
 
-	private void startHotspot() {
-		WifiApManager mgr = new WifiApManager(ctx);
-		savedConfig = mgr.getWifiApConfiguration();
-		mgr.setWifiApEnabled(config, true);
-	}
+        server = new Server();
+        server.addConnectedUsersListener(callback);
+        server.start();
+    }
 
-	private void stopHotspot() {
-		WifiApManager mgr = new WifiApManager(ctx);
-		mgr.setWifiApEnabled(config, false);
-		mgr.setWifiApConfiguration(savedConfig);
-	}
+    public void stop() {
 
-	public ServerState getServerState() {
-		return state;
-	}
+        stopHotspot();
+        stopRedirection();
+        setServerState(ServerState.STATE_OFF);
+        server.stopRun();
+        server = null;
+    }
+
+    public void setServerState(ServerState state) {
+        this.state = state;
+        callStateChangeListeners();
+    }
+
+    public ServerState getServerState() {
+        return state;
+    }
+
+    public boolean addStateChangedListener(Callback c) {
+        return statusChangeListeners.add(c);
+    }
+
+    public boolean removeStateChangedListener(Callback c) {
+        return statusChangeListeners.remove(c);
+    }
+
+    private void startRedirection() {
+        try {
+            String iptables = loadIptables();
+            StringBuilder script = new StringBuilder();
+            script.append(iptables).append(" --version\n");
+            // TODO Add lines !!!!
+
+            int res = runScript(script.toString());
+            if (res != 1) {
+                Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Log.e(this.getClass().getName(), e.toString());
+        }
+    }
+
+    private void stopRedirection() {
+        try {
+            String iptables = loadIptables();
+            StringBuilder script = new StringBuilder();
+            script.append(iptables).append(" --version\n");
+
+            // TODO Add lines !!!!
+
+            int res = runScript(script.toString());
+            if (res != 1) {
+                Toast.makeText(ctx, R.string.error_redirect, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Log.e(this.getClass().getName(), e.toString());
+        }
+    }
+
+    private String loadIptables() throws IOException {
+        File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+
+        File f = new File(tmpFolder, IPTABLES);
+        f.setExecutable(true);
+        f.deleteOnExit();
+
+        InputStream is = ctx.getResources().openRawResource(R.raw.iptables);
+        byte[] buff = new byte[is.available()];
+        is.read(buff);
+        is.close();
+
+        FileOutputStream out = new FileOutputStream(f);
+        out.write(buff);
+        out.close();
+        return f.getAbsolutePath();
+    }
+
+    private int runScript(String script) throws IOException, InterruptedException {
+
+        File tmpFolder = ctx.getDir("tmp", Context.MODE_PRIVATE);
+
+        File f = new File(tmpFolder, TEMP_SCRIPT);
+        f.setExecutable(true);
+        f.deleteOnExit();
+
+        // Write the script to be executed
+        PrintWriter out = new PrintWriter(new FileOutputStream(f));
+        if (new File("/system/bin/sh").exists()) {
+            out.write("#!/system/bin/sh\n");
+        }
+        out.write(script);
+        if (!script.endsWith("\n")) {
+            out.write("\n");
+        }
+        out.write("exit\n");
+        out.flush();
+        out.close();
+        Process exec = Runtime.getRuntime().exec("su -c " + f.getAbsolutePath());
+        return exec.waitFor();
+    }
+
+    private void startHotspot() {
+        WifiApManager mgr = new WifiApManager(ctx);
+        savedConfig = mgr.getWifiApConfiguration();
+        mgr.setWifiApEnabled(config, true);
+    }
+
+    private void stopHotspot() {
+        WifiApManager mgr = new WifiApManager(ctx);
+        mgr.setWifiApEnabled(config, false);
+        mgr.setWifiApConfiguration(savedConfig);
+    }
+
+    private void callStateChangeListeners() {
+        for (Callback c : statusChangeListeners) {
+            c.call(getServerState());
+        }
+    }
 }
--- trunk/src/com/piratebox/widget/P1R4T3B0XWidget.java
+++ trunk/src/com/piratebox/widget/P1R4T3B0XWidget.java
@@ -3,6 +3,7 @@ package com.piratebox.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -10,21 +11,34 @@ import android.widget.RemoteViews;
 import com.piratebox.R;
 import com.piratebox.System;
 import com.piratebox.System.ServerState;
+import com.piratebox.utils.Callback;
 
 public class P1R4T3B0XWidget extends AppWidgetProvider {
 
 	private static final String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+	private static boolean addedListenner = false;
 	
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		
-		System system = System.getInstance(context);
 		final int N = appWidgetIds.length;
 		RemoteViews views = new RemoteViews(context.getPackageName(),
 				R.layout.widget);
 		
-		system.updateWidgets();
+		if (!addedListenner) {
+	        System system = System.getInstance(context);
+    		system.addStateChangedListener(new Callback() {
+                @Override
+                public void call(Object arg) {
+                    ServerState state = (ServerState) arg;
+                    int[] ids = appWidgetManager.getAppWidgetIds(new ComponentName(context, P1R4T3B0XWidget.class));
+                    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+                    views.setTextViewText(R.id.widgetlabel, context.getString(state.val()));
+                    appWidgetManager.updateAppWidget(ids, views);
+                }
+            });
+		}
 		
 		for (int i = 0; i < N; i++) {
             int appWidgetId = appWidgetIds[i];
